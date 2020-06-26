da0bba821e64dd37a0bc45f548d61b17e1b5415e
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-06-25 20:14:05 -0400

    Changing Data with CRUD

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -13,16 +13,36 @@ export const addnewProduct = (req, res) => {
 export const getProducts = (req, res) => {
 
     Product.find({}, (err, Product) => {
-        if (!req.body)
-            throw err;
+        if (!req.body) throw err;
         res.json(Product);
     });
 }
 
 export const getProductWithID = (req, res) => {
     Product.findById(req.params.ProductID, (err, Product) => {
-        if(!req.body)
-            throw err;
+        if(!req.body) throw err;
         res.json(Product);
     });
+}
+
+export const updateProduct = (req, res) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, {new: true, useFindAndModify: false}, (err, Product) => {
+        if(!req.body){
+            throw err;
+        } else {
+            res.json(Product);
+        }
+            
+        
+    });
+}
+
+export const deleteProduct = (req, res) => {
+    Product.deleteOne({ _id: req.params.ProductID }, (err) => {
+        if(!req.body){
+            throw err;
+        } else {
+            res.json('successfully deleted product');
+        }
+    })
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
-import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID, updateProduct, deleteProduct } from '../controllers/controllers';
 
 export const routes = (app) => {
     app.route('/products');
     app.get(getProducts);
     app.route('/products/:ProductID');
     app.get(getProductWithID);
+    app.delete(deleteProduct);
 }
 
 routes.post(addnewProduct);
+routes.put(updateProduct);
